b83078cf12119a65349b9c7f7a49c6c3e967a2fe
Carlos yaco <cyaco33@gmail.com>
2021-05-11 10:08:32 -0500

    developed cache

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,8 @@ const express = require('express');
 const MoviesService = require('../services/movies');
 const{movieIdSchema,createMovieSchema,updateMovieSchema}=require('../utils/schemas/movies')
 const validationHandler = require('../utils/middleware/validationHandler');
+const cacheResponse = require('../utils/cacheResponse');
+const {FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS}=require('../utils/times');
 
 function moviesApi (app){
     const router =express.Router();
@@ -9,6 +11,7 @@ function moviesApi (app){
     const moviesService = new MoviesService;
 
     router.get('/',async function(req,res,next){
+        cacheResponse(res,FIVE_MINUTES_IN_SECONDS);
         const {tags} = req.query;
         try{
             const movies = await moviesService.getMovies({tags});
@@ -21,6 +24,7 @@ function moviesApi (app){
         }
     });
     router.get('/:movieId', validationHandler({movieId:movieIdSchema},'params'),async function(req,res,next){
+        cacheResponse(res,SIXTY_MINUTES_IN_SECONDS);
         const {movieId} = req.params;
         try{
             const movies = await moviesService.getMovie({movieId})
